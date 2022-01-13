fdfa71f823d83657b227bc077f11f9b3f3960d9b
Carla Fonseca <carlafb11@gmail.com>
2022-01-13 00:59:05 -0600

    Slice de resultados en homepage

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const cardsContainer = document.getElementById("cards-container")
 const prev = document.querySelector("#prev")
 const next = document.querySelector("#next")
+const seeMore = document.querySelector("#see-more")
 let currentPage = 1
 let lastPage = 0
-
+let itemsHomepage = []
 
 const getInfo = () => {
   fetch(`https://rickandmortyapi.com/api/character?page=${currentPage}`) 
@@ -13,9 +14,32 @@ const getInfo = () => {
     lastPage = data.info.pages
     const newData = data.results
     fillCards(newData)
-    console.log(newData)
+    itemsHomepage = newData.slice(0,4)
+    fillHomepageCards(itemsHomepage)
+
+    seeMore.onclick = () => {
+      fillCards(newData)
+
+    }
+
 })
 }
+
+fillHomepageCards = (data) => {
+  let htmlHolder = ""
+  data.map((item) => {
+    htmlHolder += `
+      <div id="cards-container">
+        <div class="card">
+          <p>${item.name}</p>
+          <img src="${item.image}"/>
+        </div>
+      </div>
+    `
+    cardsContainer.innerHTML = htmlHolder
+  })
+}
+
 
 const fillCards = (data) => {
   let htmlHolder = ""
@@ -28,25 +52,35 @@ const fillCards = (data) => {
         </div>
       </div>
     `
-    console.log(item)
     cardsContainer.innerHTML = htmlHolder
   })
 }
 
 getInfo()
 
+prevValidation = () => {
+  if (currentPage === 1) {
+    prev.disabled = true
+  }
+}
+
+prevValidation()
+
 next.onclick = () => {
   currentPage++
   if (currentPage === lastPage) {
     next.disabled = true
+  }
+  if (currentPage > 1) {
+    prev.disabled = false
   }
   getInfo()
 }
 
 prev.onclick = () => {
   currentPage--
-  if (currentPage === 1) {
+  if (currentPage <= 1) {
     prev.disabled = true
-  } 
+  }
   getInfo()
 }
